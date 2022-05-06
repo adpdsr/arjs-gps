915874958b445cda23092a0a1d8b6d6f4867bb0c
Arthur du Peloux <arthur.peloux@gmail.com>
2022-05-06 15:18:15 +0200

    Update places.js

--- places.js
+++ places.js
@@ -1,1 +1,50 @@
+const loadPlaces = function (coords) {
+    const PLACES = [
+        {
+            name: "Place 1",
+            location: {
+                lat: 47.230213,
+                lng: -1.571853,
+            }
+        },
+    ];
 
+    return Promise.resolve(PLACES);
+};
+
+window.onload = () => {
+    const scene = document.querySelector('a-scene');
+
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
+
+        // than use it to load from remote APIs some places nearby
+        loadPlaces(position.coords)
+            .then((places) => {
+                places.forEach((place) => {
+                    const latitude = place.location.lat;
+                    const longitude = place.location.lng;
+
+                    // add place name
+                    const text = document.createElement('a-link');
+                    text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    text.setAttribute('title', place.name);
+                    text.setAttribute('href', 'http://www.example.com/');
+                    text.setAttribute('scale', '20 20 20');
+
+                    text.addEventListener('loaded', () => {
+                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                    });
+
+                    scene.appendChild(text);
+                });
+            })
+    },
+        (err) => console.error('Error in retrieving position', err),
+        {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 27000,
+        }
+    );
+};
